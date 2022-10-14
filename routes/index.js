f4d28c0f1bcbe179e7449bd66a32545cb6a4fe89
ahmadmurteza <59716267+ahmadmurteza@users.noreply.github.com>
2022-10-15 02:30:18 +0800

    comment
    crud di table comment dan menampilkan comment pada photo

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const usersRouter = require("./users-router");
 const photosRouter = require("./photo-routes");
+const commentRouter = require("./comment-routes");
 const errorMiddleware = require("./../middleware/error-middleware");
 const authenticationMiddleware = require("./../middleware/authentication-middleware");
 
@@ -9,6 +10,7 @@ router.use("/users",usersRouter);
 
 router.use(authenticationMiddleware);
 router.use("/photos", photosRouter);
+router.use("/comments", commentRouter);
 
 router.use((req, res, next) => {
 	next({ name: "PageNotFound" });
